eb21868d17b38306d11dee534cf460926ae8988a
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-07-11 11:01:26 -0500

    modified Home page

--- src/pages/Public/HomePage/index.js
+++ src/pages/Public/HomePage/index.js
@@ -1,13 +1,14 @@
-import { Banner, SectionPatient, ServicesGrid } from './components';
+import { Banner, SectionPatient, ServicesGrid, Offices } from './components';
 
 const HomePage = () => {
     return (
         <>
             <Banner />
-            <h2 id="services" className="title-section">Especialidades</h2>
+            <h2 id="services" className="title-section">Servicios Dentales</h2>
             <div className="mx-auto underline"></div>
-            <ServicesGrid />
-            <SectionPatient />
+            <ServicesGrid /><br/><br/>
+            <SectionPatient /><br/><br/>
+            <Offices /><br/><br/>
         </>
     );
 }
