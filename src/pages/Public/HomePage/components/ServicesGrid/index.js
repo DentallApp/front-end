564d89d056fa28e62ce5d1e1ebddf597476c2f9c
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-07-11 11:06:47 -0500

    Added card carousel

--- src/pages/Public/HomePage/components/ServicesGrid/index.js
+++ src/pages/Public/HomePage/components/ServicesGrid/index.js
@@ -1,14 +1,34 @@
-import services from '../../../../../data/services';
+import { useContext } from 'react';
+import Carousel from 'react-elastic-carousel';
+import GeneralTreamentsContext from '../../../../../context/GeneralTreamentsContext';
 import ServiceCard from '../ServiceCard';
 import styles from './ServicesGrid.module.css';
 
 const ServicesGrid = () => {
+    const breakPoints = [
+        { width: 1, itemsToShow: 1 },
+        { width: 550, itemsToShow: 2 },
+        { width: 850, itemsToShow: 3 },
+        { width: 1150, itemsToShow: 4 },
+        { width: 1450, itemsToShow: 5 },
+        { width: 1750, itemsToShow: 6 },
+    ];
+
+    const { getGeneralTreatmentAll } = useContext(GeneralTreamentsContext);
+    let generalTreatments = getGeneralTreatmentAll();
+  
     return (
-        <ul className={styles.services_list}>
-            {services.map(service => (
-                <ServiceCard key={service.id} service={service} />
-            ))}
-        </ul>
+        <>
+            {generalTreatments != null ? (
+                <Carousel breakPoints={breakPoints} className={`mx-auto ${styles.container}`}>
+                    {generalTreatments.map(service => (
+                        <ServiceCard key={service.id} service={service} />
+                    ))}
+                </Carousel>
+            ):
+                <div>Cargando</div>
+            }
+        </>
     );
 }
 
