128e609aa6d8558faf98eaa26839479f6f62774b
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-07-11 11:02:23 -0500

    Added the component Offices

--- src/pages/Public/HomePage/components/index.js
+++ src/pages/Public/HomePage/components/index.js
@@ -1,3 +1,4 @@
 export { default as Banner } from './Banner';
 export { default as ServicesGrid } from './ServicesGrid';
 export { default as SectionPatient } from './SectionPatient';
+export { default as Offices } from './Offices';
