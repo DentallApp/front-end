d8cd4a28fe6e2398ba75fa5e367a3d30f057dd12
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-07-26 17:26:58 -0500

    Fixed infinite rendering bug

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,10 +1,17 @@
-import { Navigate, Outlet } from "react-router-dom"
+import { useEffect } from 'react'; 
+import { Navigate, Outlet, useLocation, useNavigate } from "react-router-dom"
 import { getLocalUser } from '../services/UserService';
 
-
-
 const PrivateRoute = ({ role }) => {
+    const navigate = useNavigate()
     const user = getLocalUser();
+    const location = useLocation();
+    const unProtectedPaths = ["/login"];
+
+    useEffect(() => {
+        if(user === null || user === undefined) navigate("/login", {state:location});
+    }, [navigate, location, user]);
+
     return (
         <>
             {
@@ -17,13 +24,14 @@ const PrivateRoute = ({ role }) => {
                             <Navigate to="/no-autorizado" />
                         )
                     ) : 
-                    (
-                        <Navigate to="/login" replace={true}/>
-                    )
-                ) : 
-                (
-                    <Navigate to="/login" replace={true}/>
-                )
+                        ((!unProtectedPaths.includes(location.pathname)) ? 
+                        (<Navigate to="/login" state={{from: location}} />) : (<Navigate to="/login" state={{from: location}} />))
+                   
+                ): (
+                
+                    !unProtectedPaths.includes(location.pathname) ? 
+                    (<Navigate to="/login" state={{from: location}} />) : (<Navigate to="/login" state={{from: location}} />))
+                
             }
         </>
     ) 
--- src/routes/PublicRoute.js
+++ src/routes/PublicRoute.js
@@ -1,18 +1,27 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import { useEffect } from 'react'; 
+import { Outlet, useLocation, useNavigate, Navigate } from 'react-router-dom';
 import { getLocalUser } from '../services/UserService';
 
-const user = getLocalUser();
 
 const PublicRoute = () => {
+
+    const navigate = useNavigate()
+    const user = getLocalUser();
+    const location = useLocation();
+    const protectedPaths = ["/inicio"];
+
+    useEffect(() => {
+        if(user !== null) navigate("/inicio", {state:location});
+    }, [navigate, location, user]);
+    
     return (
         <>
             {
                 user === null || user === undefined ? (
-                    <>
-                        <Outlet />
-                    </>
+                    <Outlet />
                 ):(
-                    <Navigate to="/inicio" replace={true}/>
+                    !protectedPaths.includes(location.pathname) ? 
+                    (<Navigate to="/inicio" state={{from: location}} />) : (<Navigate to="/inicio" state={{from: location}} />)
                 )
             }
         </>
