886fe948db8660e9a837c1a488c9a0bd360ecc49
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-07-26 05:35:15 -0500

    Added AuthService

---
+++ src/services/AuthService.js
@@ -0,0 +1,28 @@
+import api from './Api';
+import { removeLocalUser, setLocalUser } from './UserService';
+
+export const login = ({ userName, password }) => {
+    
+    return api.post('/login', {
+        userName,
+        password
+    }).then(res => {
+        if(res.data.success) 
+            setLocalUser({...res.data.data});
+        
+        return {
+            success: res.data.success,
+            message: res.data.message
+        };
+    })
+    .catch(err => {
+        return {
+            success: err.response.data.success,
+            message: err.response.data.message
+        };
+    })
+}
+
+export const logout = () => {
+    removeLocalUser();
+}
