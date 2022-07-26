2607fc5c94718a4d0180aa2f28ca55d4a9758686
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-07-26 05:37:54 -0500

    Added UserService

---
+++ src/services/UserService.js
@@ -0,0 +1,37 @@
+import api from './Api';
+
+export const setLocalUser = (user) => {
+    localStorage.setItem('user', JSON.stringify(user));
+}
+
+export const getLocalUser = () => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    return user;
+}
+
+export const removeLocalUser = () => {
+    localStorage.removeItem("user");
+}
+
+export const registerBasicUser = (data) => {
+    return api.post('/register/basic-user', {
+        ...data
+    })
+    .then(res => {
+        if(res.data.success === true) {
+            console.log(res.data);
+            return {
+                success: res.data.success,
+                message: res.data.message
+            };
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        return {
+            success: err.response.data.success,
+            message: err.response.data.message
+        };
+    })
+
+}
