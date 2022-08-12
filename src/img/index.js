42bc3412fc534fd0ba478481a6e2954ca3954f9a
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-08-12 16:20:23 -0500

    Added new gif

--- src/img/index.js
+++ src/img/index.js
@@ -3,3 +3,4 @@ export { default as dentalBanner } from './dental-baner.png';
 export { default as imageAbout} from './world_dental_about.jpg';
 export { default as user } from './user.png';
 export { default as check } from './check.png';
+export { default as asistente_robot } from './asistente_robot.gif';
