b60f299810c8a01fe020a3e6681ea5bc7ff8b623
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-08-12 16:16:04 -0500

    Added option for basic user

--- src/components/NavBarDashboard/index.js
+++ src/components/NavBarDashboard/index.js
@@ -1,14 +1,16 @@
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import{ NavDropdown } from 'react-bootstrap';
-import SideBarContext from '../../context/SideBarContext';
-import styles from './NavBarDashboard.module.css';
 import { ImMenu } from "react-icons/im";
-import { user } from '../../img';
+import SideBarContext from '../../context/SideBarContext';
+import { getLocalUser } from '../../services/UserService';
+import ROLES from '../../constants/Roles';
 import { logout } from '../../services/AuthService';
+import { user } from '../../img';
+import styles from './NavBarDashboard.module.css';
 
 const NavBarDashboard = () => {
-
+    const userData = getLocalUser();
     const { handleSidebar, onlyWidth } = useContext(SideBarContext);
 
     return (
@@ -21,13 +23,21 @@ const NavBarDashboard = () => {
                 ) }
                 <div className={styles.user}>
                     <NavDropdown title={<img src={user} alt="avatar"/>} id="collasible-nav-dropdown">
-                        <NavDropdown.Item as={Link} to="/configuracion">Configurar</NavDropdown.Item>
+                        { userData.roles.includes(ROLES.BASIC_USER) && (
+                            <NavDropdown.Item 
+                            as={Link} 
+                            to="/configuracion" onClick={() => handleSidebar()}>
+                                Configurar
+                            </NavDropdown.Item>
+                        ) }
                         <NavDropdown.Item 
                         as={Link} to="/login" onClick={() => {
                         handleSidebar();
                         logout();
                         window.scrollTo({ top: 0, behavior: 'smooth' })
-                        }}>Salir</NavDropdown.Item>
+                        }}>
+                            Salir
+                        </NavDropdown.Item>
                     </NavDropdown> 
                 </div>
             </nav>
