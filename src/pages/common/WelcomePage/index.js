3b672199c5bf479dae33573468b1499c91f9527c
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-08-12 16:21:44 -0500

    Added welcome page

---
+++ src/pages/common/WelcomePage/index.js
@@ -0,0 +1,19 @@
+import { getLocalUser } from '../../../services/UserService';
+import { asistente_robot } from '../../../img';
+import styles from './WelcomePage.module.css'
+
+const WelcomePage = () => {
+
+    const user = getLocalUser();
+
+    return (
+        <div className={styles.container_page}>
+            <h1 className={styles.page_title}>{ user.genderId === 1 ? 'Bienvenido' : 'Bienvenida'}</h1>
+            <div className="underline mx-auto"></div>
+            <h2 className={styles.fullname_user}>{`${user.names} ${user.lastNames}`}</h2>
+            <img className={styles.gif} src={asistente_robot} alt="Asistente robot" />
+        </div>
+    );
+}
+
+export default WelcomePage;
