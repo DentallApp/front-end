527836e9fab4cbc804acb4ebab49150ddf4ff94d
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-08-12 16:22:42 -0500

    Added WelcomePage and ChangePasswordPage

--- src/pages/common/index.js
+++ src/pages/common/index.js
@@ -1,5 +1,7 @@
 export { default as NotFoundPage } from './NotFoundPage';
 export { default as UnauthorizedPage } from './UnauthorizedPage';
 export { default as Dashboard } from './Dashboard';
+export { default as WelcomePage } from './WelcomePage';
+export { default as ChangePasswordPage } from './ChangePasswordPage';
 export { default as UserManagementPage } from './UserManagementPage';
 export { default as SessionExpiredPage } from './SessionExpiredPage';
