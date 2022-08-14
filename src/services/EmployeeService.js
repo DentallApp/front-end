d78f3828f19971ee9777177cad8768ddf5689ee7
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-08-14 15:32:20 -0500

    Added EmployeeService

---
+++ src/services/EmployeeService.js
@@ -0,0 +1,110 @@
+import api from './Api';
+
+export const getEmployee = async () => {
+    return await api.get('/employee');
+}
+
+export const createEmployee = (data) => {
+    return api.post('/register/employee', {
+        document: data.document,
+        names: data.names,
+        lastNames: data.lastNames,
+        cellPhone: data.cellPhone,
+        dateBirth: data.dateBirth,
+        genderId: data.genderId,
+        userName: data.email,
+        password: data.password,
+        officeId: data.officeId,
+        pregradeUniversity: data.pregradeUniversity,
+        postgradeUniversity: data.postgradeUniversity,
+        roles: data.roles
+    })
+    .then(res => {
+        if(res.data.success === true) {
+            return {
+                success: res.data.success,
+                message: res.data.message
+            }
+        }
+    })
+    .catch(err => {
+        return {
+            success: err.response.data.success,
+            message: err.response.data.message
+        };
+    })
+}
+
+export const updateEmployee = (data) => {
+    return api.put(`/employee/${data.employeeId}`, {
+        document: data.document,
+        names: data.names,
+        lastNames: data.lastNames,
+        cellPhone: data.cellPhone,
+        dateBirth: data.dateBirth,
+        genderId: data.genderId,
+        email: data.email,
+        officeId: data.officeId,
+        pregradeUniversity: data.pregradeUniversity,
+        postgradeUniversity: data.postgradeUniversity,
+        roles: data.roles
+    })
+    .then(res => {
+        if(res.data.success === true) {
+            return {
+                success: res.data.success,
+                message: res.data.message
+            }
+        }
+    })
+    .catch(err => {
+        return {
+            success: err.response.data.success,
+            message: err.response.data.message
+        };
+    })
+}
+
+export const deleteEmployee = (id) => {
+    return api.delete(`/employee/${id}`)
+    .then(res => {
+        if(res.data.success === true) {
+            return {
+                success: res.data.success,
+                message: res.data.message
+            }
+        }
+    })
+    .catch(err => {
+        return {
+            success: err.response.data.success,
+            message: err.response.data.message
+        };
+    })
+}
+
+export const updateProfileEmployee = (data) => {
+    return api.put('/employee', {
+        names: data.names,
+        lastNames: data.lastNames,
+        cellPhone: data.cellPhone,
+        dateBirth: data.dateBirth,
+        genderId: data.genderId,
+        pregradeUniversity: data.pregradeUniversity,
+        postgradeUniversity: data.postgradeUniversity,
+    })
+    .then(res => {
+        if(res.data.success === true) {
+            return {
+                success: res.data.success,
+                message: res.data.message
+            }
+        }
+    })
+    .catch(err => {
+        return {
+            success: err.response.data.success,
+            message: err.response.data.message
+        };
+    })
+}
