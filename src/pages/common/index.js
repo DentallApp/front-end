fe59e8220907fbedab8a68235ab610bf33b1f4ec
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-08-14 15:30:24 -0500

    Added export of profile editing page

--- src/pages/common/index.js
+++ src/pages/common/index.js
@@ -4,4 +4,5 @@ export { default as Dashboard } from './Dashboard';
 export { default as WelcomePage } from './WelcomePage';
 export { default as ChangePasswordPage } from './ChangePasswordPage';
 export { default as UserManagementPage } from './UserManagementPage';
+export { default as EditProfilePage } from './EditProfilePage';
 export { default as SessionExpiredPage } from './SessionExpiredPage';
