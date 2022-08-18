74cd9a2b4c9387278bddb51376a8a4817b34b497
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-08-18 09:55:59 -0500

    Added option for treatment management

--- src/components/SideBar/SuperadminOptions.js
+++ src/components/SideBar/SuperadminOptions.js
@@ -1,5 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import { TbDental } from "react-icons/tb";
+import { RiHealthBookFill } from "react-icons/ri";
 import styles from './SideBar.module.css';
 
 const SuperadminOptions = () => {
@@ -10,6 +11,12 @@ const SuperadminOptions = () => {
             onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
                 <TbDental className={styles.icon} /> 
                 Servicios
+            </NavLink>
+            <NavLink className={({ isActive }) => isActive ? styles.navlink_active : styles.navlink } 
+            to="gestion-tratamientos" 
+            onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+                <RiHealthBookFill className={styles.icon} /> 
+                Tratamientos
             </NavLink>
         </>
     );
