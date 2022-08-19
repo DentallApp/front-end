b1a48572b8cb170158756f07a84b3561c966890c
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-08-19 10:28:00 -0500

    Added getGeneralTreatmentName function

--- src/pages/SuperAdmin/TreatmentPage/components/FormModal/index.js
+++ src/pages/SuperAdmin/TreatmentPage/components/FormModal/index.js
@@ -3,6 +3,7 @@ import { Form, Button, Container, Row, Col, Modal } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
 import { useWindowWidth } from '@react-hook/window-size';
 import { formatPrice } from '../../../../../utils/formatUtils';
+import { getGeneralTreatmentName } from '../../../../../services/GeneralTreatments';
 import styles from './FormModal.module.css';
 
 const formatter = new Intl.NumberFormat('en-US', {
@@ -10,25 +11,45 @@ const formatter = new Intl.NumberFormat('en-US', {
     maximumFractionDigits: 2
 });
 
-const FormModal = ({show, handleClose, treatmentSelect = null, saveTreatment}) => {
+const FormModal = ({show, handleClose, specificTreatmentSelect = null, saveTreatment}) => {
     
+    const [services, setServices] = useState(null);
     const onlyWidth = useWindowWidth(); // Se obtiene ancho y altura de pantalla para colocar el modal
     const [type, setType] = useState('create');
 
-    const { register, handleSubmit, reset, formState: {errors} } = useForm({
+    const { register, handleSubmit, reset, setValue, watch, formState: {errors} } = useForm({
         defaultValues: {
-            id: `${ treatmentSelect !== null ? treatmentSelect.id : ""}`,
-            name: `${ treatmentSelect !== null ? treatmentSelect.name : ""}`,
-            price: `${ treatmentSelect !== null ? formatter.format(treatmentSelect.price) : ""}`,
+            specificTreatmentId: `${ specificTreatmentSelect !== null ? specificTreatmentSelect.specificTreatmentId : ""}`,
+            name: `${ specificTreatmentSelect !== null ? specificTreatmentSelect.specificTreatmentName : ""}`,
+            price: `${ specificTreatmentSelect !== null ? formatter.format(specificTreatmentSelect.price) : ""}`,
+            generalTreatmentId: `${ specificTreatmentSelect !== null ? specificTreatmentSelect.generalTreatmentId : ""}`,
         }
     });
 
+    const selectValue = watch("generalTreatmentId");
+
     useEffect(() => {
-        if(treatmentSelect !== null) {
+        register("generalTreatmentId", { required: "Consultorio requerido" });   
+
+        getGeneralTreatmentName().then(res => {
+            setServices(res.data);
+        })
+        .catch(err => err); 
+
+        if(specificTreatmentSelect !== null) {
+            setValue("generalTreatmentId", specificTreatmentSelect.generalTreatmentId, true);
             setType('edit');
         }
+        
         // eslint-disable-next-line react-hooks/exhaustive-deps      
     }, []);
+
+    useEffect(() => {
+        if(services !== null && selectValue === '') setValue("generalTreatmentId", services[0].id, true);
+         // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [services]);
+
+    const handleChange = (e) => setValue("generalTreatmentId", e.target.value, true);
 
     return (
         <Modal 
@@ -39,7 +60,7 @@ const FormModal = ({show, handleClose, treatmentSelect = null, saveTreatment}) =
         fullscreen={ onlyWidth < 768 ? true : false} 
         >
             <Modal.Header className={styles.container_header} closeButton>
-                <Modal.Title>{ treatmentSelect === null ? 'Crear Tratamiento' : 'Editar información' }</Modal.Title>
+                <Modal.Title>{ specificTreatmentSelect === null ? 'Crear Tratamiento' : 'Editar información' }</Modal.Title>
             </Modal.Header>
             <Modal.Body>
                 <Form 
@@ -65,12 +86,14 @@ const FormModal = ({show, handleClose, treatmentSelect = null, saveTreatment}) =
                                     { errors.name && <p className={styles.error_message}>{ errors.name.message }</p> } 
                                 </Form.Group>
                             </Col>
+                        </Row>
 
+                        <Row>
                             <Col lg={12} md>
                                 <Form.Group className="mb-3" controlId="formBasicPrice">
                                     <Form.Label className={styles.label_input}>* Precio</Form.Label>
                                     <Form.Control 
-                                    
+                                    placeholder="Ingrese precio"
                                     {...register("price", {
                                         required: "Precio es requerido",
                                         pattern: {
@@ -83,6 +106,28 @@ const FormModal = ({show, handleClose, treatmentSelect = null, saveTreatment}) =
                                         },
                                     })} />
                                     { errors.price && <p className={styles.error_message}>{ errors.price.message }</p> }
+                                </Form.Group>
+                            </Col>
+
+                            <Col lg={12} md>
+                                <Form.Group className="mb-3" controlId="formBasicService">
+                                    <Form.Label className={styles.label_input}>* Servicios</Form.Label>
+                                    <Form.Select
+                                    name="generalTreatmentId"
+                                    value={selectValue} 
+                                    onChange={handleChange}
+                                    >
+                                        { services && (
+                                            services.map(data => (
+                                                <option 
+                                                key={data.id} 
+                                                value={data.id}>
+                                                    {data.name}
+                                                </option>
+                                            ))
+                                        ) }
+                                    </Form.Select>
+                                    { errors.generalTreatmentId && <p className={styles.error_message}>{ errors.generalTreatmentId.message }</p> }
                                 </Form.Group>
                             </Col>
                         </Row>
