f55af9e37eba88f8ee375a491feb43614840c46f
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-08-19 10:33:03 -0500

    Added SpecificTreatmentService

---
+++ src/services/SpecificTreatmentService.js
@@ -0,0 +1,79 @@
+import api from './Api';
+
+export const getSpecificTreatment = async() => {
+    return await api.get('/specific-treatment');
+}
+
+export const createSpecificTreatment = (data) => {
+    return api.post('/specific-treatment', {
+        name: data.name,
+        generalTreatmentId: data.generalTreatmentId,
+        price: data.price
+    })
+    .then(res => {
+        if(res.data.success === true) {
+            return {
+                status: res.status,
+                success: res.data.success,
+                message: res.data.message
+            }
+        }
+    })
+    .catch(err => {
+        if(err.response.status === 0) return {status: err.response.status,}
+        
+        return {
+            status: err.response.status,
+            success: err.response.data.success,
+            message: err.response.data.message
+        };
+    })
+}
+
+export const updateSpecificTreatment = (data) => {
+    return api.put(`/specific-treatment/${data.specificTreatmentId}`, {
+        name: data.name,
+        generalTreatmentId: data.generalTreatmentId,
+        price: data.price
+    })
+    .then(res => {
+        if(res.data.success === true) {
+            return {
+                status: res.status,
+                success: res.data.success,
+                message: res.data.message
+            }
+        }
+    })
+    .catch(err => {
+        if(err.response.status === 0) return {status: err.response.status,}
+        
+        return {
+            status: err.response.status,
+            success: err.response.data.success,
+            message: err.response.data.message
+        };
+    })
+}
+
+export const deleteSpecificTreatment = (id) => {
+    return api.delete(`/specific-treatment/${id}`)
+    .then(res => {
+        if(res.data.success === true) {
+            return {
+                status: res.status,
+                success: res.data.success,
+                message: res.data.message
+            }
+        }
+    })
+    .catch(err => {
+        if(err.response.status === 0) return {status: err.response.status,}
+        
+        return {
+            status: err.response.status,
+            success: err.response.data.success,
+            message: err.response.data.message
+        };
+    })
+}
