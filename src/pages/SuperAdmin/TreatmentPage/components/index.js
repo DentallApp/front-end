8dadf21870bf96c0e83b0afb085b67f0eea914f6
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-08-19 10:29:49 -0500

    Added SelectGneeralService component

--- src/pages/SuperAdmin/TreatmentPage/components/index.js
+++ src/pages/SuperAdmin/TreatmentPage/components/index.js
@@ -1,3 +1,4 @@
 export { default as TreatmentTable } from './TreatmentTable';
 export { default as FormModal } from './FormModal';
 export { default as EliminationModal } from './EliminationModal';
+export { default as SelectGeneralService } from './SelectGeneralService';
