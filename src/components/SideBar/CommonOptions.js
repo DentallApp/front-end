1cbae173c69a2baa21f62091d4b95df6b4528313
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-08-26 18:39:31 -0500

    Added option for office management

--- src/components/SideBar/CommonOptions.js
+++ src/components/SideBar/CommonOptions.js
@@ -1,25 +1,67 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { FaUsers } from "react-icons/fa";
+import { AiOutlineCaretDown,AiOutlineCaretUp } from "react-icons/ai";
+import { FaTasks } from 'react-icons/fa';
+import { BsFillCalendar2WeekFill } from "react-icons/bs";
+import { ImOffice } from "react-icons/im";
 import ROLES from '../../constants/Roles';
 import { getLocalUser } from '../../services/UserService';
 import styles from './SideBar.module.css';
 
 const CommonOptions = () => {
-
+    const [submenuShow, setSubmenuShow] = useState(false);
     const user = getLocalUser();
 
     return (
         <>
             { 
                 (user.roles.includes(ROLES.ADMIN) || user.roles.includes(ROLES.SUPERADMIN)) &&  
-                <NavLink className={({ isActive }) => isActive ? styles.navlink_active : styles.navlink } 
-                to={'gestion-usuarios'} 
-                onClick={() => {window.scrollTo({ top: 0, behavior: 'smooth' })} }>
-                    <FaUsers className={styles.icon} /> 
-                    Usuarios
-                </NavLink>
+                <>
+                    <NavLink className={({ isActive }) => isActive ? styles.navlink_active : styles.navlink } 
+                    to={'gestion-usuarios'} 
+                    onClick={() => {window.scrollTo({ top: 0, behavior: 'smooth' })} }>
+                        <FaUsers className={styles.icon} /> 
+                        Usuarios
+                    </NavLink>
+
+                    <NavLink className={styles.navlink }
+                    to="#"
+                    onClick={() => {setSubmenuShow(!submenuShow)} }>
+                        <div className={styles.container_submenu}>
+                            <div style={{display: 'flex', alignItems: 'center'}}>
+                                <ImOffice className={styles.icon} /> 
+                                Consultorios
+                            </div>
+                            { !submenuShow ? 
+                                <AiOutlineCaretDown className={styles.icon} /> 
+                                : <AiOutlineCaretUp className={styles.icon}/> 
+                            } 
+                        </div>
+                    </NavLink>
+                    { submenuShow && (
+                        <ul className={styles.submenu}>
+                            {
+                                (user.roles.includes(ROLES.SUPERADMIN)) && (
+                                    <NavLink className={({ isActive }) => isActive ? styles.navlink_active : styles.navlink }
+                                    to="consultorios/gestion"
+                                    onClick={() => {window.scrollTo({ top: 0, behavior: 'smooth' })} }>
+                                        <FaTasks className={styles.icon} />
+                                        Gestion
+                                    </NavLink>
+                                ) 
+                            }
+                                   
+                            <NavLink className={({ isActive }) => isActive ? styles.navlink_active : styles.navlink } 
+                            to="consultorios/horarios"
+                            onClick={() => {window.scrollTo({ top: 0, behavior: 'smooth' })} }>
+                                <BsFillCalendar2WeekFill className={styles.icon} /> 
+                                Horarios de atención
+                            </NavLink>      
+                        </ul>
+                    ) }
+                </>
             }
-            
         </>
     );
 }
