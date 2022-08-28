cce07a1c5c0f1f2cfee50e034f38bc15515cd985
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-08-28 14:12:00 -0500

    Added MedicalDirectoryPage

--- src/pages/Patient/index.js
+++ src/pages/Patient/index.js
@@ -2,4 +2,5 @@ export { default as AppointmentChatbotPage } from './AppointmentChatbotPage';
 export { default as AppointmentHistory } from './AppointmentHistory';
 export { default as DependentPage } from './DependentPage';
 export { default as QuotationPage } from './QuotationPage';
+export { default as MedicalDirectoryPage } from './MedicalDirectoryPage';
 export { default as EditProfilePage } from './EditProfilePage';
