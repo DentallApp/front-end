b0bd1953a2eb3716efcf5f6d4fd5ffca562a00cb
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-08-28 20:50:42 -0500

    Added FavoriteDentistPage

--- src/pages/Patient/index.js
+++ src/pages/Patient/index.js
@@ -3,4 +3,5 @@ export { default as AppointmentHistory } from './AppointmentHistory';
 export { default as DependentPage } from './DependentPage';
 export { default as QuotationPage } from './QuotationPage';
 export { default as MedicalDirectoryPage } from './MedicalDirectoryPage';
+export { default as FavoriteDentistPage } from './FavoriteDentistPage';
 export { default as EditProfilePage } from './EditProfilePage';
