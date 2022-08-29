bed6b196ad5067195a2c5263e4d4d21f789da738
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-08-29 09:06:06 -0500

    Added statuses for the alert message and the loading modal

--- src/pages/common/ScheduleManagementPage/index.js
+++ src/pages/common/ScheduleManagementPage/index.js
@@ -1,14 +1,17 @@
 import { useState, useEffect } from 'react';
 import { Button, Spinner } from 'react-bootstrap';
 import { IoAddCircle } from "react-icons/io5";
-import moment from 'moment';
 import { ScheduleTable, FilterDentist, FormModal } from './components';
+import { AlertMessage, ModalLoading } from '../../../components';
 import { validationScheduleMorning, validationScheduleAfternoon } from './utils';
-import data from './data';
+import { getDentists, getSchedulesByEmployee, createSchedule, updateSchedule } from '../../../services/DentistScheduleService';
 import styles from './ScheduleManagementPage.module.css';
 
 const ScheduleManagementPage = () => {
 
+    const [errorLoading, setErrorLoading] = useState({success: false, message: ''});
+
+    const [dentists, setDentists] = useState(null);
     const [selectedDentist, setSelectedDentist] = useState(null);
     const [schedules, setSchedules] = useState(null);
     const [selectedSchedule, setSelectedSchedule] = useState(null);
@@ -17,8 +20,15 @@ const ScheduleManagementPage = () => {
     const [show, setShow] = useState(false);
     const [rowSelect, setRowSelect] = useState(null);
 
+    // Estado para el mensaje de alerta
+    const [alert, setAlert] = useState(null);
+
+    // Estado para el modal de carga 
+    const [isLoading, setIsLoading] = useState(null);
+
     useEffect(() => {
-        setSchedules(data);
+        getDentists().then(res => setDentists(res.data))
+            .catch(err => err);
     }, []);
 
     // Funciones para cerrar y mostrar el modal
@@ -28,11 +38,75 @@ const ScheduleManagementPage = () => {
     }
     const handleShow = () => setShow(true);
 
-    const saveSchedule = (data, reset, type, setError) => {
-        console.log(data);
-        if(parseInt(data.dayId) === 0) {
-            setError("dayId", {
-                type: 'custom',
+    const showSchedules = (employeeId) => {
+        if(parseInt(employeeId) !== 0) {
+            getSchedulesByEmployee(parseInt(employeeId))
+                .then(res => setSchedules(res.data))
+                .catch(err => handleErrorLoading(err));
+        }
+    }
+
+    const handleSelectDentist = (e) => {
+        setSelectedDentist(parseInt(e.target.value));
+        
+        showSchedules(e.target.value);
+    }
+
+    const handleErrors = (result) => {
+        if(result.success === undefined && (result.status === 0 || result.status === 400 || 
+            result.status === 404 || result.status === 405 ||
+            result.status === 500)) {
+            setAlert({success: false, message: 'Error inesperado. Refresque la página o intente más tarde'});
+            setIsLoading({success: false});
+        }
+    }
+
+    const handleErrorLoading = (err) => {
+        if((err.response.status === 0 && err.response.data === undefined) || 
+                (err.response.data.success === undefined && (err.response.status === 400 
+                || err.response.status === 405 ||
+                err.status === 500))) {
+                setErrorLoading({success: true, message: 'Error inesperado. Refresque la página o intente más tarde'});
+                return;
+        }  
+        setErrorLoading({success: true, message: err.response.data.message});
+    }
+
+    const create = async (data) => {
+        data.employeeId = parseInt(selectedDentist);
+
+        const result = await createSchedule(data);
+        if(result.success && result.success === true) showSchedules(data.employeeId);
+
+        setIsLoading({success: result.success});
+        setAlert(result);
+
+        return result;
+    }
+
+    const update = async (data) => {
+        data.scheduleId = parseInt(data.scheduleId);
+        data.employeeId = parseInt(selectedDentist);
+        data.morningStartHour = data.morningStartHour === 'null' ? null : data.morningStartHour;
+        data.morningEndHour = data.morningEndHour === 'null' ? null : data.morningEndHour;
+        data.afternoonStartHour = data.afternoonStartHour === 'null' ? null : data.afternoonStartHour;
+        data.afternoonEndHour = data.afternoonEndHour === 'null' ? null : data.afternoonEndHour;
+        data.isDeleted = parseInt(data.statusId) === 1 ? false : true;
+        
+        const result = await updateSchedule(data);
+        if(result.success && result.success === true) showSchedules(data.employeeId);
+
+        setIsLoading({success: result.success});
+        setAlert(result);
+
+        return result;
+    }
+
+    const saveSchedule = async(data, reset, type, setError) => {
+        
+        if(parseInt(data.weekDayId) === 0) {
+            setError("weekDayId", {
+                type: 'custom5',
                 message: 'Seleccione un día'
             });
             return;
@@ -41,8 +115,15 @@ const ScheduleManagementPage = () => {
         const validationMorning = validationScheduleMorning(data, setError);
         const validationAfternoon = validationScheduleAfternoon(data, setError);
 
-        if(validationMorning === false || validationAfternoon === false) return;
+        if(validationMorning === false) return;
+        if(validationAfternoon === false) return;
+        
+        let result = null;
 
+        if(type === 'create') result = await create(data);
+        else result = await update(data);
+        
+        handleErrors(result);
         handleClose();
         reset();
         setSelectedSchedule(null);
@@ -50,34 +131,75 @@ const ScheduleManagementPage = () => {
 
     return (
         <>
+            { isLoading ? (isLoading.success === undefined ? <ModalLoading show={true} /> : "") : ""}
             { /* Ventana modal para el registro, actualización y eliminación de dependiente  */
                 show === true && (
                     <FormModal
                     handleClose={handleClose} 
                     show={show}
                     specificTreatmentSelect={rowSelect}
+                    schedules={schedules}
                     scheduleSelect={selectedSchedule}
                     saveSchedule={saveSchedule} />         
                 )
             }    
             <h1 className={styles.page_title}>Gestión de Horarios</h1>
+            { /* Mensaje de alerta para mostrar información al usuario */
+                alert && 
+                <div className={styles.container_alert}>
+                    <AlertMessage 
+                    type={ alert.success === false ? 'danger' : 'success' }
+                    message={ alert.message }
+                    setError= { setAlert }  /> 
+                </div>
+            }
 
             <div className={styles.container_header}>
-                <FilterDentist setSelectedDentist={setSelectedDentist} />
+                <FilterDentist 
+                dentists={dentists}
+                selectedDentist={selectedDentist}
+                handleSelectDentist={handleSelectDentist} 
+                />
                 
-                <Button 
-                className={styles.button_add} 
-                onClick={() => {
-                    handleShow();
-                }}> 
-                    <IoAddCircle className={styles.icon} /> Agregar horario
-                </Button>
-            </div>    
-            <ScheduleTable 
-            schedules={data}
-            setSelectedSchedule={setSelectedSchedule}
-            handleShow={handleShow}
-             />
+                { 
+                    (selectedDentist !== 0 && selectedDentist !== null) ? (
+                        <Button 
+                        className={styles.button_add} 
+                        onClick={() => {
+                            handleShow();
+                        }}>
+                            <IoAddCircle className={styles.icon} /> Agregar horario
+                        </Button> 
+                    ):<></>
+                }    
+            </div>
+
+            {
+                selectedDentist === 0 || selectedDentist === null ? (
+                    <p className={styles.message_information}>No hay datos para mostrar</p>
+                ):(
+                    
+                    errorLoading.success === false ? (
+                            schedules ? (
+                                <ScheduleTable 
+                                schedules={schedules}
+                                setSelectedSchedule={setSelectedSchedule}
+                                handleShow={handleShow}
+                                />
+                            ):(
+                                <div className={`${styles.container_spinner}`}>
+                                    <Spinner size="lg" className={styles.spinner} animation="border" variant="info" />
+                                    <p className={styles.text_loading}>Cargando...</p>
+                                </div>
+                            )
+                    ):(
+                        <h4 className={styles.text_error}>
+                            {errorLoading.message}
+                        </h4>
+                    )
+                )
+            }    
+            
         </>
     );
 }
