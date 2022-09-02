123ba6edc7c89073180bf932648965315ac158e3
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-09-02 17:28:00 -0500

    Created OfficeScheduleService

---
+++ src/services/OfficeScheduleService.js
@@ -0,0 +1,59 @@
+import api from './Api';
+
+export const getAllSchedules = async() => {
+    return await api.get('/office-schedule');
+}
+
+export const getScheduleByOfficeId = async(officeId) => {
+    return await api.get(`/office-schedule/${officeId}`);
+}
+
+export const createSchedule = (data) => {
+    return api.post('/office-schedule', {
+        weekDayId: data.weekDayId,
+        officeId: data.officeId,
+        startHour: data.startHour,
+        endHour: data.endHour
+    })
+    .then(res => {
+        return {
+            status: res.status,
+            success: res.data.success,
+            message: res.data.message
+        }
+    })
+    .catch(err => {
+        if(err.response.status === 0) return {status: err.response.status,}
+
+        return {
+            status: err.response.status,
+            success: err.response.data.success,
+            message: err.response.data.message
+        }
+    })
+}
+
+export const updateSchedule = (data) => {
+    return api.put(`/office-schedule/${data.scheduleId}`, {
+        weekDayId: data.weekDayId,
+        startHour: data.startHour,
+        endHour: data.endHour,
+        isDeleted: data.isDeleted
+    })
+    .then(res => {
+        return {
+            status: res.status,
+            success: res.data.success,
+            message: res.data.message
+        }
+    })
+    .catch(err => {
+        if(err.response.status === 0) return {status: err.response.status,}
+
+        return {
+            status: err.response.status,
+            success: err.response.data.success,
+            message: err.response.data.message
+        }
+    })
+}
