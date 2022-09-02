c8576432f65d7dca96d990a233c2799fc3ed4ce1
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-09-01 21:23:13 -0500

    Added OfficeManagementPage

--- src/pages/SuperAdmin/index.js
+++ src/pages/SuperAdmin/index.js
@@ -1,2 +1,3 @@
 export { default as GeneralServicePage } from './GeneralServicePage';
 export { default as TreatmentPage } from './TreatmentPage';
+export { default as OfficeManagementPage } from './OfficeManagementPage';
