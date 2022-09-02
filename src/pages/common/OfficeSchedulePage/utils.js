a98aec791889697bfb6ffa2bad6e8eabb9ba3c54
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-09-02 17:26:40 -0500

    Changed names of data object properties

--- src/pages/common/OfficeSchedulePage/utils.js
+++ src/pages/common/OfficeSchedulePage/utils.js
@@ -1,13 +1,13 @@
 export const validationSchedule = (data, setError) => {
     let validation = true;
 
-    if(data.startTime > data.endTime) {
-        setError("startTime", {
+    if(data.startHour > data.endHour) {
+        setError("startHour", {
             type: 'custom',
             message: 'Hora de inicio no puede ser mayor a la hora fin'
         });
 
-        setError("endTime", {
+        setError("endHour", {
             type: 'custom',
             message: 'Hora fin no puede ser menor a la hora de inicio'
         });
