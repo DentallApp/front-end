bf598501e27c3f1b1d72388d51555db2b5ca606c
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-09-03 21:59:28 -0500

    Added specific height to the front card

--- src/pages/Public/HomePage/components/OfficeCard/index.js
+++ src/pages/Public/HomePage/components/OfficeCard/index.js
@@ -19,21 +19,21 @@ const OfficeCard = ({office}) => {
             <FrontSide style={{"boxShadow": "none"}} >
                 <Card className={`${styles.card_office}`} style={{ width: '18rem' }}>
                     <ImOffice className={styles.icon}/>
-                        <h5>{office.name}</h5>
-                        <br></br>
-                        <div style={{"height":"100px"}}>
-                            <h6><FaMapMarkerAlt /> Dirección</h6>
-                            <p>{office.address}</p>
-                        </div>    
-                            <br />    
-                            <h6><FaPhone /> Contacto</h6>
-                            <p>{office.contactNumber ? office.contactNumber : 'N/A'}</p>
+                    <h5>{office.name}</h5>
+                    <br></br>
+                    <div style={{"height":"100px"}}>
+                        <h6><FaMapMarkerAlt /> Dirección</h6>
+                        <p>{office.address}</p>
+                    </div>    
+                    <br />    
+                    <h6><FaPhone /> Contacto</h6>
+                    <p>{office.contactNumber ? office.contactNumber : 'N/A'}</p>
                         
-                        <Button 
-                        className={styles.button_view_schedule} 
-                        onClick={() => onRotate() }> 
-                            <FaCalendarAlt /> Ver Horarios
-                        </Button>
+                    <Button 
+                    className={styles.button_view_schedule} 
+                    onClick={() => onRotate() }> 
+                        <FaCalendarAlt /> Ver Horarios
+                    </Button>
                 </Card>
                 
             </FrontSide>
