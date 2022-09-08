ebb22f32ccca1f9411ae1d1b06056ab1124cd7f5
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-09-07 20:55:48 -0500

    Added validRange property

--- src/pages/common/EmployeeAppointmentView/components/WebCalendar/index.js
+++ src/pages/common/EmployeeAppointmentView/components/WebCalendar/index.js
@@ -3,9 +3,22 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 import esLocale from '@fullcalendar/core/locales/es';
+import moment from 'moment';
 import styles from '../MobileCalendar/AppointmentCalendar.module.css';
 
-const WebCalendar = ({events, setAppointmentSelect, handleShow, setEventClassNames}) => {
+const WebCalendar = ({
+    events, 
+    setAppointmentSelect, 
+    handleShow, 
+    setEventClassNames,
+    startDate,
+    endDate
+}) => {
+
+    const getStartDate = () => startDate;
+    
+    const getEndDate = () => moment(endDate).add(1, 'days').format('yyyy-MM-DD');
+
     return (
         <FullCalendar
         initialView= {'dayGridMonth'}
@@ -30,7 +43,11 @@ const WebCalendar = ({events, setAppointmentSelect, handleShow, setEventClassNam
         }}
         eventClassNames= {(arg) => setEventClassNames(arg, styles)}
         slotMinTime={'9:00:00'}
-        slotMaxTime={'20:00:00'}
+        slotMaxTime={'20:30:00'}
+        validRange={{
+            start: getStartDate(),
+            end: getEndDate()
+        }}
         eventClick={function(arg) {
             setAppointmentSelect(arg);
             handleShow();
