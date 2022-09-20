6ae0a3d5397f650d8b15b5651898b28b6d19bf23
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-09-20 12:04:22 -0500

    Added reporting options for administrator and super administrator

--- src/components/SideBar/CommonOptions.js
+++ src/components/SideBar/CommonOptions.js
@@ -3,7 +3,14 @@ import { NavLink } from 'react-router-dom';
 import { FaUsers } from "react-icons/fa";
 import { AiOutlineCaretDown,AiOutlineCaretUp } from "react-icons/ai";
 import { FaTasks } from 'react-icons/fa';
-import { BsFillCalendar2WeekFill, BsCalendarWeekFill, BsFillCalendarWeekFill, BsFillCalendar2XFill } from "react-icons/bs";
+import { 
+    BsFillCalendar2WeekFill, 
+    BsCalendarWeekFill, 
+    BsFillCalendarWeekFill, 
+    BsFillCalendar2XFill,
+    BsFillCheckCircleFill
+} from "react-icons/bs";
+import { FaClipboardList } from "react-icons/fa";
 import { ImOffice } from "react-icons/im";
 import { IoCalendarNumber } from "react-icons/io5";
 import ROLES from 'constants/Roles';
@@ -13,6 +20,7 @@ import styles from './SideBar.module.css';
 const CommonOptions = () => {
     const [submenuShow, setSubmenuShow] = useState(false);
     const [submenuAppointment, setSubmenuAppointment] = useState(false);
+    const [submenuReports, setSubmenuReports] = useState(false);
     const user = getLocalUser();
 
     return (
@@ -37,14 +45,16 @@ const CommonOptions = () => {
                     </NavLink>
                     { submenuAppointment && (
                         <ul className={styles.submenu}>
-                            <NavLink className={({ isActive }) => isActive ? styles.navlink_active : styles.navlink }
+                            <NavLink className={({ isActive }) => isActive ? 
+                            `${styles.navlink_active}` : `${styles.navlink}` }
                             to="citas/calendario"
                             onClick={() => {window.scrollTo({ top: 0, behavior: 'smooth' })} }>
                                 <BsFillCalendarWeekFill className={styles.icon} />
                                 Calendario
                             </NavLink>
                                    
-                            <NavLink className={({ isActive }) => isActive ? styles.navlink_active : styles.navlink } 
+                            <NavLink className={({ isActive }) => isActive ? 
+                            `${styles.navlink_active}` : `${styles.navlink}` } 
                             to="citas/cancelacion"
                             onClick={() => {window.scrollTo({ top: 0, behavior: 'smooth' })} }>
                                 <BsFillCalendar2XFill className={styles.icon} /> 
@@ -111,6 +121,50 @@ const CommonOptions = () => {
                     <BsCalendarWeekFill className={styles.icon} /> 
                     Horarios
                 </NavLink>
+            }
+
+{ 
+                ( user && (user.roles.includes(ROLES.ADMIN) || user.roles.includes(ROLES.SUPERADMIN))) &&  
+                <>
+                    <NavLink className={styles.navlink }
+                    to="#"
+                    onClick={() => {setSubmenuReports(!submenuReports)} }>
+                        <div className={styles.container_submenu}>
+                            <div style={{display: 'flex', alignItems: 'center'}}>
+                                <FaClipboardList className={styles.icon} /> 
+                                Reportes
+                            </div>
+                            { !submenuReports ? 
+                                <AiOutlineCaretDown className={styles.icon} /> 
+                                : <AiOutlineCaretUp className={styles.icon}/> 
+                            } 
+                        </div>
+                    </NavLink>
+                    { submenuReports && (
+                        <ul className={styles.submenu}>        
+                            <NavLink className={({ isActive }) => isActive ? styles.navlink_active : styles.navlink } 
+                            to="reportes/citas"
+                            onClick={() => {window.scrollTo({ top: 0, behavior: 'smooth' })} }>
+                                <BsFillCheckCircleFill className={styles.icon} style={{'fontSize': '42px !important'}}/> 
+                                Citas asistidas, no asistidas y canceladas
+                            </NavLink>
+
+                            <NavLink className={({ isActive }) => isActive ? styles.navlink_active : styles.navlink } 
+                            to="reportes/citas-agendadas"
+                            onClick={() => {window.scrollTo({ top: 0, behavior: 'smooth' })} }>
+                                <BsFillCheckCircleFill className={styles.icon}/> 
+                                Citas agendadas
+                            </NavLink>
+
+                            <NavLink className={({ isActive }) => isActive ? styles.navlink_active : styles.navlink } 
+                            to="reportes/ranking-servicios-dentales"
+                            onClick={() => {window.scrollTo({ top: 0, behavior: 'smooth' })} }>
+                                <BsFillCheckCircleFill className={styles.icon}/> 
+                                Servicios dentales por citas atendidas
+                            </NavLink>         
+                        </ul>
+                    ) }
+                </>
             }
 
         </>
