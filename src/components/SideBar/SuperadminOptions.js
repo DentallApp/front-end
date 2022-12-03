5939e49e4404e20dffa179588c8cbb15b7724471
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-12-02 20:12:42 -0500

    Added option in the sidebar for the holidays option

--- src/components/SideBar/SuperadminOptions.js
+++ src/components/SideBar/SuperadminOptions.js
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import { TbDental } from "react-icons/tb";
 import { RiHealthBookFill } from "react-icons/ri";
+import { FaBookmark } from "react-icons/fa";
 import SideBarContext from 'context/SideBarContext';
 import styles from './SideBar.module.css';
 
@@ -28,6 +29,15 @@ const SuperadminOptions = () => {
             }}>
                 <RiHealthBookFill className={styles.icon} /> 
                 Tratamientos
+            </NavLink>
+            <NavLink className={({ isActive }) => isActive ? styles.navlink_active : styles.navlink } 
+            to="configuracion-feriados" 
+            onClick={() => {
+                handleSidebar(false);
+                window.scrollTo({ top: 0, behavior: 'smooth' })
+            }}>
+                <FaBookmark className={styles.icon} /> 
+                Feriados
             </NavLink>
         </>
     );
