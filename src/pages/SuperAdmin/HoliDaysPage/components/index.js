21fd3e6421a8555fe45237d1f3db5655d022600c
MrDave1999 <daveseva2010@hotmail.es>
2023-01-01 12:52:19 -0500

    Removed the option to filter offices on the public holidays page

--- src/pages/SuperAdmin/HoliDaysPage/components/index.js
+++ src/pages/SuperAdmin/HoliDaysPage/components/index.js
@@ -1,4 +1,3 @@
 export { default as HoliDaysTable } from './HoliDaysTable';
 export { default as FormModal } from './FormModal';
-export { default as OfficeFilter } from './OfficeFilter';
 export { default as EliminationModal } from './EliminationModal';
